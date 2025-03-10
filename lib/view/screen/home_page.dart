8c3138f90722bc79f30c435fbdf7cc4a83fceb11
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-03-10 00:48:15 -0700

    first commit

--- lib/view/screen/home_page.dart
+++ lib/view/screen/home_page.dart
@@ -6,7 +6,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("data"),),
+      appBar: AppBar(
+        title: Text("Home Page"),
+        centerTitle: true,
+      ),
+
     );
   }
 }
