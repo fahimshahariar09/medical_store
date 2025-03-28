b1ba8f740cf80af56167e3a00049fee2f9921095
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2025-03-27 19:41:45 -0700

    first commit

--- lib/view/screen/home_page.dart
+++ lib/view/screen/home_page.dart
@@ -13,6 +13,7 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           Text("data"),
+          Text("data"), Text("data"),  Text("data"),
           Text("data"), Text("data"),
 
 
